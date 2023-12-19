6333ac713d3f3fcbcdabf8e7d4d87b4ae809f1ac
Rong Ding <116621544+sam880629@users.noreply.github.com>
2023-12-19 12:15:12 +0800

    Add files via upload

---
+++ function_type.ts
@@ -0,0 +1,44 @@
+
+// -函式宣告
+function fuc1(x: number, y: number): number{
+     return x+y;
+}
+
+// !輸入多餘的引數，是不被允許的
+// sum_fuc(1, 2, 3);
+
+// -函式表示式
+let mySum = function(x: number, y: number): number{
+    return x+y;
+}
+
+console.log(mySum(1,5));
+
+
+function fuc2(x: number | string): number | string {
+    if (typeof x === 'number') {
+        return Number(x.toString().split('').reverse().join(''));
+    } else if (typeof x === 'string') {
+        return x.split('').reverse().join('');
+    }
+   
+}
+
+console.log(fuc2(123));      // 输出 321
+console.log(fuc2('hello'));  // 输出 'olleh'
+
+
+//! 可選引數必須接在必須引數後面
+
+function buildName(firstName: string, lastName?: string) {
+    if (lastName) {
+        return firstName + ' ' + lastName;
+    } else {
+        return firstName;
+    }
+}
+
+let tomcat = buildName('Tom', 'Cat');
+let tom = buildName('Tom');
+
+
